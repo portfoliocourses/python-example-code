8ebff439d36471faffb34a9218e181319b018bac
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-05-26 20:51:27 -0400

    check if a number is prime or not

---
+++ check_prime.py
@@ -0,0 +1,82 @@
+################################################################################
+#
+# Program: Check If A Number Is Prime
+# 
+# Description: Example of checking if a number is prime using Python.
+#
+# YouTube Lesson: https://www.youtube.com/watch?v=ZP0QcJrclE4 
+#
+# Author: Kevin Browne @ https://portfoliocourses.com
+#
+################################################################################
+
+
+# prompt the user to enter a number, store the number as an int value
+number = int(input("Enter Number: "))
+
+# start off with the assumption the number *is* prime and we'll try to show that
+# it is not prime
+is_prime = True
+
+# A prime number *must* be > 1
+if number > 1:
+  
+  # A prime number only has 1 and itself as factors, no other factors between 
+  # 2 ... number-1 should be found otherwise the number is NOT prime.  A factor 
+  # is a number that divides another number with no remainder.  So in this loop
+  # we check all possible divisors between 2 and number-1 to see if they are 
+  # factors.
+  for divisor in range(2, number):
+
+    # The modulus operator % returns the remainder of number divided by divisor,
+    # so if the remainder is 0 we know that the divisor is a factor and that the 
+    # number is NOT prime.  So we set is_prime to false and stop checking for 
+    # factors because finding one is enough to demonstrate the number is not 
+    # prime!
+    if (number % divisor) == 0:
+      is_prime = False
+      break
+   
+  # If after checking all the numbers between 2 and number-1 and NONE of them 
+  # are divisors, is_prime will still be true (from when we set it above) and 
+  # we can say the number MUST be prime.  Otherwise if we found a factor then 
+  # is_prime will be set to false and we can report the number is NOT prime.
+  if is_prime:
+    print(number, "is prime")
+  else:
+    print(number, "is not prime")
+
+# if the number is not greater than 1, inform the user 
+else:
+  print("Number must be > 1")
+
+
+# We can also define a function that checks whether a number is prime and 
+# returns true if the number is prime and false otherwise.  
+def check_prime(number):
+  
+  # The algorithm to determine whether the number is prime is exactly the same 
+  # as above, just done within a function!
+  is_prime = True
+  if number > 1:
+    for divisor in range(2, number):
+      if (number % divisor) == 0:
+        is_prime = False
+        break
+
+    # Instead of outputting whether the number is prime, we return true/false
+    # whether the number is prime or not
+    return is_prime
+  
+  # if the number is not > 1 we know it is not primve
+  else:
+    return False
+
+
+# test the function, 7 should be prime
+if (check_prime(7)):
+  print("7 is prime")
+
+# test the function, 6 should NOT be prime
+if (not check_prime(6)):
+  print("6 is NOT prime")
