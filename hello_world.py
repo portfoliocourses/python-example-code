872cf868340acd297a75d45e393190c83b245bd4
Portfolio Courses <85077613+portfoliocourses@users.noreply.github.com>
2022-09-04 17:31:38 -0400

    hello, world program

---
+++ hello_world.py
@@ -0,0 +1,19 @@
+################################################################################
+#
+# Program: Hello World
+#
+# Description: A "Hello, World" program in Python.  See Hello, World programs
+# Wikipedia article - https://en.wikipedia.org/wiki/%22Hello,_World!%22_program
+#
+# YouTube Lesson: https://www.youtube.com/watch?v=hp4pYFASTrc
+#
+# Author: Kevin Browne @ https://portfoliocourses.com
+#
+################################################################################
+
+
+# Output Hello, World
+
+print("Hello, World")
+
+print("Hello, World AGAIN")
